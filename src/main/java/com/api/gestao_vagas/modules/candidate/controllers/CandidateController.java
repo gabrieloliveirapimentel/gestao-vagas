abbfd4858519de5c13a0f432878d4bbfb4eff146
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-10-30 08:57:34 -0300

    Feat: Criando repositório de candidato

--- src/main/java/com/api/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/api/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,21 +1,25 @@
 package com.api.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.gestao_vagas.modules.candidate.CandidateEntity;
+import com.api.gestao_vagas.modules.candidate.CandidateRepository;
 
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
     
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidate) {
-        // Lógica para criar um novo candidato
-        System.out.println("Candidato criado: " + candidate.getName());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidate) {
+        return this.candidateRepository.save(candidate);
     }
 }

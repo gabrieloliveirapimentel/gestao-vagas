42945d6c1ef26aa92453b434d960ed95a990e41c
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-10-30 09:37:45 -0300

    Feat: Criando entidade Company

---
+++ src/main/java/com/api/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,42 @@
+package com.api.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Entity(name = "companies")
+@Data
+public class CompanyEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "O campo [username] não deve estar em branco")
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um email valido")
+    private String email;
+
+    @Length(min = 8, max = 32, message = "O campo [password] deve conter entre 8 e 32 caracteres")
+    private String password;
+    
+    private String website;
+    private String name;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}

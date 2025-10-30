e9cb8773f44ef5ba77b76c04ba4d4921365e9aa8
Gabriel Pimentel <gabrieloliveirapimentel@hotmail.com>
2025-10-30 08:52:18 -0300

    Feat: Criando tabela de candidato

--- src/main/java/com/api/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/api/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,18 +1,28 @@
 package com.api.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidates")
 public class CandidateEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    
     private String name;
 
     @NotBlank(message = "O campo [username] não deve estar em branco")
@@ -24,6 +34,10 @@ public class CandidateEntity {
 
     @Length(min = 8, max = 32, message = "O campo [password] deve conter entre 8 e 32 caracteres")
     private String password;
+    
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
